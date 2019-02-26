d38abd085214a75a9f3a23d6936f439d72a8558d
delta12 <delta12@pop-os.localdomain>
2019-02-26 17:22:48 -0500

    First Commit

---
+++ OnlineGame/OnlineGame-Windows.cpp
@@ -0,0 +1,12 @@
+#include <fstream>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  for(int a=0;a<10;a++)	//Change a<10 to how many times you want it to perform
+  {
+    system("start http://google.com");	//Change google with a URL you want
+
+  }
+}
