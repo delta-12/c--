9d787d9cb48d162e24d9db5e4dce0fe297d42cae
delta-12 <40147170+delta-12@users.noreply.github.com>
2019-02-27 18:27:54 -0500

    Create sample2.cpp

---
+++ autocompile/Windows/source/sample2.cpp
@@ -0,0 +1,9 @@
+#include <fstream>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  system("echo Hello World!");
+  system("echo This is some more sample source code and another sample .exe binary.");
+}
