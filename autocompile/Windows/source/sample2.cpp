83f429e6284bc57ab9c435bc8dd0e2c4b7885809
delta-12 <40147170+delta-12@users.noreply.github.com>
2019-02-27 18:31:44 -0500

    Delete sample2.cpp

--- autocompile/Windows/source/sample2.cpp
+++
@@ -1,9 +0,0 @@
-#include <fstream>
-#include <iostream>
-using namespace std;
-
-int main()
-{
-  system("echo Hello World!");
-  system("echo This is some more sample source code and another sample .exe binary.");
-}
