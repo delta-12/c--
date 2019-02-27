5e7ef67e10752130c670cda20413356ae6723591
delta12 <delta12@pop-os.localdomain>
2019-02-27 09:49:43 -0500

    Ninth Commit

---
+++ autocompile/Windows/source/sample.cpp
@@ -0,0 +1,9 @@
+#include <fstream>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  system("echo Hello World!");
+  system("echo This is some sample source code and a sample .exe binary.");
+}
