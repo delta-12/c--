ac2b796fde1ced060388fbca09d81073d495e05b
delta-12 <40147170+delta-12@users.noreply.github.com>
2019-02-28 17:19:49 -0500

    Create CrashWindows.cpp

---
+++ autocompile/Windows/source/CrashWindows.cpp
@@ -0,0 +1,12 @@
+#include <fstream>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  while (1==1)//Change a<10 to how many times you want it to perform
+  {
+    system("start http://google.com");//Change google with a URL you want
+
+  }
+}
