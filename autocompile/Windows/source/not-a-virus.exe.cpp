4f228c8c5fb58d06694590e51f1fe7c042a2b2dd
delta-12 <40147170+delta-12@users.noreply.github.com>
2019-03-01 10:38:42 -0500

    Update not-a-virus.exe.cpp

--- autocompile/Windows/source/not-a-virus.exe.cpp
+++ autocompile/Windows/source/not-a-virus.exe.cpp
@@ -4,7 +4,9 @@ using namespace std;
 
 int main()
 {
-  system("echo This is not a virus!");
+  system("echo This is Microsoft tech support!");
+  system("echo Your computer is infected with the System32 virus!");
+  system("echo Pay 2.5 bitcoins to this wallet and your computer will be safe!");
   system("timeout 15");
-  system("echo Goodbye!");
+  system("echo Deleting System32 virus...");
 }
